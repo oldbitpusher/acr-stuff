607e071150b550ded0723f708e8d23a05eb532e6
clyde <clyde@dta-105.localdomain>
2018-10-13 20:53:54 -0500

    Completed rework to do "new item" refresh without re-execing self

--- ebay-sale-check.sh
+++ ebay-sale-check.sh
@@ -5,9 +5,6 @@
 # Searching EBAY can be done via the URL and does not require authentication
 # maybe unless you were hitting hit hundreds of time ber second :-)
 #
-# A bit of mungling $IFS, usually to make bash do tokenizing 
-# on specified characters
-#
 # Usage: $0 [search item]
 #
 PS4='$LINENO: '
@@ -20,25 +17,37 @@ set -u			# Complain about unset variables
 # &rt=nc&
 # LH_ItemCondition=4	Pre-owned/used
 
+#
+# Template for EBAY search URI
+#
 declare -r SEARCH="https://www.ebay.com/sch/i.html?_nkw=%s&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=4"
 
 declare    OPT_DEBUG=false
-declare    OPT_LOG=''
 
-declare -A descriptions
-declare -A priceList
-declare -A itemList
-declare -A itemKeys
-declare    searchItem
+#
+# Size of alert message box
+#
+declare -r MSGBOX_W=30 
+declare -r MSGBOX_H=10 
 
-declare -r -i DI_OK=0
-declare -r -i DI_EXTRA=3
-declare -r -i DI_ESCAPE=255`
+#
+# Return codes from dialog
+#
+export DIALOG_OK=0
+export DIALOG_CANCEL=1
+export DIALOG_HELP=2
+export DIALOG_EXTRA=3
+export DIALOG_HELP_ITEM=4
+export DIALOG_ESC=5
+export DIALOG_ERROR=6
 
-declare -r -i RC_OK=0
-declare -r -i RC_HARD_ERROR=1
-declare -r -i RC_SOFT_ERROR=2
-declare -r -i RC_RESET=3
+#
+# print message to stderr in debug mode
+#
+function debug()
+{
+	$OPT_DEBUG && echo "debug: $*" 1>&2
+}
 
 #
 # Round - take DD.cc and round to nearest DD
@@ -51,12 +60,6 @@ function round()
 	cents=${cents#0}
 	[[ $cents -gt 50 ]] && ((++dol))
 	echo $dol
-}
-
-function debug()
-{
-	$OPT_DEBUG && echo "debug: $*" 1>&2
-	[[ -n ${OPT_LOG:-} ]] && echo "debug: $*" 1>&2 >> $OPT_LOG
 }
 
 #
@@ -74,6 +77,7 @@ function url_encode() {
 }
 
 #
+#
 # What a sales record looks like:
 # (rubbish)
 # (rubbish)
@@ -83,42 +87,22 @@ function url_encode() {
 #> $56.00
 # (rubbish)
 # 
-
-#
-# next_line - iterate to the next line from lineBuffer
-#
-
-
-
-
-function doit()
+function price_tag()
 {
-	local -i currentPos=0
-	local    currentLine
-	local -a lineBuffer=()
-	local -i currentPos=0		# Current line 
-	local    currentLine=''		# Current line content
-	local -i lineCount
-
-	function next_line()
-	{
-		[[ $currentPos -ge ${#inputLines[@]} ]] && return 1
-		((++currentPos))
-		currentLine="${lineBuffer[$currentPos]:-}"
-		return 0
-	}
+	local searchItem=''
 
 	if [[ -n ${1:-} ]]; then 
-		local searchItem="$@"
+		searchItem="$@"
 	else
-		local searchItem="$(dialog --title "Enter item" \
+		searchItem="$(dialog --title "Enter item" \
 			--clear \
 			--inputbox "Enter item to search for" \
 			10 50 2>&1 1>&3)"
-		rc=$?
-		if [[ -n ${searchItem:-} ]]; then
-			echo "No search item"
-			exit 0
+		local rc=$?
+		if [[ -z ${searchItem:-} ]]; then
+			debug "No search item"
+			dialog --msgbox "No search Item" $MSGBOX_H $MSGBOX_W
+			return 2
 		fi
 	fi
 	debug "Search item \"$searchItem\""
@@ -128,128 +112,143 @@ function doit()
 	# Fetch web page using "lynx -dump" - dont care about HTML, just want content
 	#
 	local -a inputLines=()
+	local -i currentLine=0
 	readarray -t inputLines <<<"$(lynx -dump -width=1024 "$SEARCH_URL" | sed 's/^[ \t]*//')"
 	debug "Query response ${#inputLines[@]} lines"
-
 	if [[ ${#inputLines[@]} -eq 0 ]]; then
 		debug "No response to query"
-		dialog --msgbox "No data"
-		return $RC_SOFT_ERROR
+		dialog --msgbox "No data" $MSGBOX_H $MSGBOX_W
+		return 1
 	fi
 	
 	local -i itemCount=0
-	local -a descriptions=()
-	local -a priceList=()
+	local -a itemDetails=()
+	local -a itemPrices=()
 
-	while next_line; do
-		if [[ currentLine =~ SOLD ]]; then
-			next_line
-			descriptions[$itemCount]=$currentLine
-			while next_line; do
-				if [[ $currentLine =~ \$[0-9]* ]]; then
-					priceList[$itemCount]="$currentLine"
-					((++itemCount))
-					break # inner while
-				fi
+	while [[ $currentLine -lt ${#inputLines[@]} ]]; do
+		if [[ ${inputLines[$currentLine]} =~ SOLD ]]; then
+			((++currentLine))
+			itemDetails[$itemCount]="${inputLines[$currentLine]}"
+			((++currentLine))
+			while [[ ! ${inputLines[$currentLine]} =~ \$[0-9]* ]] && \
+			      [[ $currentLine -lt ${#inputLines[@]} ]]; do
+				((++currentLine))
 			done
+			local t_price="${inputLines[$currentLine]/ */}"
+			t_price="${t_price//,}"
+			itemPrices[$itemCount]="${t_price}"
+			((++itemCount))
 		fi
+		((++currentLine))
 	done
-
 	if [[ $itemCount -eq 0 ]]; then
 		debug "No results for \"$searchItem\""
-		dialog -title "No resul> VARSXts" \
+		dialog  --title "No results" \
 			--msgbox "The search for \"$searchItem\" did not return any items" \
-			10 20
-		exit 0
+			$MSGBOX_H $MSGBOX_W
+		return 0
 	fi
 	debug "Found $itemCount transactions"
-	
 	#
 	# Collapse info arrays into one assoc array index by price
 	#
-	BASE=()
-	for (( ix=0 ; $ix < $itemCount; ix++ )); do
-		x_index=$(round ${priceList[$ix]/\$/})
-		itemList[$x_index]="${priceList[$ix]}@%s@${descriptions[$ix]}"
+	local -A itemList=()
+	local -A itemKeys=()
+	local -i x_item
+	for (( x_item=0; $x_item < $itemCount; x_item++ )); do
+		x_index=$(round ${itemPrices[$x_item]/\$/})
+		itemList[$x_index]="${itemPrices[$x_item]}@%s@${itemDetails[$x_item]}"
 		itemKeys[$x_index]=$x_index
 		debug "ITEM $x_index |${itemList[$x_index]}|"
 	done
+
 	#
 	# Sort prices high to low
 	#
-	IFS=' ' read -a BASE_ORDER <<<"$(echo "${!itemList[@]}" | tr ' '  '\012' | sort -r -n | tr '\012' ' ')";
-	ORDER=( ${BASE_ORDER[@]} )
-	unset IFS
-
-	debug "Base order: ${ORDER[@]}"
+	local -a sortedIndex
+	readarray -t sortedIndex <<<"$(printf -- '%s\n' ${!itemList[@]} | sort -r -n)"
+	debug "Sorted index: ${sortedIndex[@]}"
+	#
+	# Loop while the index order is changing
+	#
+	displayIndex=( ${sortedIndex[@]} )
 
 	while true; do
-		X_ORDER="${ORDER[@]}"
 		#
 		# This one-liner genrates the average of the numeric values
-		# of the elements of ${ORDER[@]} -- which are dollar-rounded prices
+		# of the elements of ${displayIndex[@]} -- which are dollar-rounded prices
 		#
-		t_ave=$(( ( $(printf -- "%s + " ${ORDER[@]}) 0 ) / ${#ORDER[@]} ))
+		local -i t_AveragePrice=$(( ( $(printf -- "%s + " ${displayIndex[@]}) 0 ) / ${#displayIndex[@]} ))
 		#
 		# Build dialog checkbox entires
 		# Each entry is weighed against the computed average
 		#
-		x_args=()
-		for x_seq in ${ORDER[@]}; do
-			t_price=${itemKeys[$x_seq]}
-			t_rank=$(( ( $t_price * 10 ) / $t_ave ))	# Rank == price percent compared to average
-			status='on'									# Hook for turning on/off based upon rank
-			t_rank="$(printf "%3d" $t_rank)"
-			t_rank=${t_rank:0:-1}.${t_rank:-1}			# Change NN to N.N
-			t_rank=${t_rank/ \./0.}						# Add '0' to sub-1 value
-			x_args+=( ${itemKeys[$x_seq]} "$(printf "${itemList[$x_seq]}" "[$t_rank]")" $status )
+		local itemDialog=()
+		for x_display in ${displayIndex[@]}; do
+			t_price=${itemKeys[$x_display]}
+			t_rank=$(( ( $t_price * 10 ) / $t_AveragePrice ))	# Rank == price percent compared to average
+			local t_status='on'									# Hook for turning on/off based upon rank
+			t_rank="$(printf "%d.%d" $(( $t_rank / 10)) $(( $t_rank % 10 )) )"
+			itemDialog+=( ${itemKeys[$x_display]} "$(printf "${itemList[$x_display]}" "[$t_rank]")" $t_status )
 		done
 		#
 		# Output of dialog is list of tags from selected rows
 		# Since the rows are sorted by price, the selection keys will
 		# be sorted high to low.
 		#
-		resp=$(dialog --column-separator @ --title "Price search $searchItem" \
+		local response=$(dialog --column-separator @ \
+			--title "Price search $searchItem" \
 			--colors \
 			--extra-button --extra-label 'New Item' \
 			--cancel-label Done \
 			--ok-label Update \
-			--checklist "Found ${#ORDER[@]} items, average price \$$t_ave" \
+			--checklist "Found ${#displayIndex[@]} items, average price \$$t_AveragePrice" \
 			$win_lines $win_cols $text_lines \
-			"${x_args[@]}" 2>&1 1>&3)
-		rcode=$?
-		# corder="${ORDER[@]}"
+			"${itemDialog[@]}" 2>&1 1>&3)
+		local rcode=$?
+		echo "DIALOG EXIT $rcode" 1>&2
 		case $rcode in
-			$DI_OK) # Pressed 'OK' button - redo unless no changes (REFRESH)
-				[[ "$resp" == "$X_ORDER" ]] && return $DI_EXTRA
-				ORDER=( $resp )
-				continue	# Big loop
+			$DIALOG_OK) # Pressed 'OK' button - redo unless no changes (REFRESH)
+				debug "DIALOG_OK"
+				t_list="${displayIndex[@]}"
+				[ "$response" == "$t_list" ] && return 0
+				displayIndex=( $response )
 				;;
-			$DI_EXTRA)	# Pressed 'EXTRA' button - reset and go again (NEW)
-				ARGS=''
-				return $DI_EXTRA
+			$DIALOG_EXTRA)	# Pressed 'EXTRA' button - reset and go again (NEW)
+				debug "DIALOG_EXTRA"
+				return 1
 				;;
-			$DI_ESCAPE)	# Pressed 'ESCAPE' button (DONE)
+			$DIALOG_ESC)	# Pressed 'ESCAPE' key
+				debug "DIALOG_ESCAPE"
 				if [[ $resp =~ Error ]]; then
-					dialog --msgbox "Error: $resp"
+					dialog --msgbox "Error: $resp" $MSGBOX_H $MSGBOX_W
 					return 
 				fi
+				exit $rcode
+				;;
+			$DIALOG_CANCEL)	# Selected "DONE" (button CANCEL) on panel
+				debug "DIALOG_DONE"
+				clear
 				exit 0
 				;;
-			*)	echo "Weird exit code from dialog: $rcode" 1>&2
+			$DIALOG_ERROR)	# Selected "DONE" (button CANCEL) on panel
+				debug "DIALOG_ERROR"
+				clear
+				exit $DIALOG_ERROR
+				;;
+			*)	debug "Weird exit code from dialog: $rcode" 
 				exit $rcode
 				;;
 		esac
 	done
 }
 
-#
+# -----------------------------------------------------------
 # MAIN
-#
-while getopts dl: x_opt; do
+# -----------------------------------------------------------
+while getopts d x_opt; do
 	case $x_opt in
 	d)	OPT_DEBUG=true;;
-	l)	OPT_LOG=$OPTARG; >$OPT_LOG;;
 	*)	;;
 	esac
 done
@@ -258,16 +257,20 @@ shift $(( $OPTIND - 1 ))
 exec 3>&1			# Set up FDs for dialog
 $OPT_DEBUG || trap 'clear' 0
 
-# Window lines => 80% screen lines
-# Window cols = 80% screen cols
-# text lines = 80% screen lines
-declare -i tty_lines=$(tput lines 2>/dev/tty)	# Need stderr > /dev/tty to get actual window size
-declare -i tty_cols=$(tput cols 2>/dev/tty)		# Need stderr > /dev/tty to get actual window size
+#
+# Compute size of dialog box and subparts
+#
+declare -i tty_lines=$(tput lines 2>/dev/tty)		# Supress any 'not found' errors
+declare -i tty_cols=$(tput cols 2>/dev/tty)
+#
+# If the script is being debugged (stderr to file) redirect stderr to /dev/tty
+#
 if [[ -z ${tty_lines:=} ]] ||  [[ -z ${tty_cols:-} ]]; then
 	read spl spv spx rl rows cl cols junk <<<"$(stty -a | grep rows)"
 	tty_lines=${rows/;//}
 	tty_cols=${cols/;//}
 fi
+
 # Lines = 90% of window height
 declare -i win_lines=$((  ( $tty_lines / 10 ) * 9 ))
 
@@ -277,10 +280,14 @@ declare -i win_cols=$((   ( $tty_cols  / 10 ) * 8 ))
 # Text lines = 90% of panel height
 declare -i text_lines=$(( ( $win_lines / 10 ) * 9 ))
 
+#
+# The first iteration will pass command line arguments
+# Subsequent iterations will pass empty argument list
+# Program exit is controlled by price_tag()
+#
 ARGS="$*"
 while true; do
-	doit $ARGS
-	[[ $? -eq $DI_EXTRA ]] ||  break
+	price_tag ${ARGS:-}
+	ARGS=''
 done
-
 # notreached
